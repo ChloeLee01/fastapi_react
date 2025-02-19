aed9cc3c26920bea617ee70fcbdb1560f10cd297
ChloeLee01 <youngsunyi01@gmail.com>
2025-02-19 22:28:37 +0900

    깃클론 후 가상환경 설정정

--- back/main.py
+++ back/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, Response
 from api.endpoints import users  # 추가된 라우터 파일
 
 # FastAPI 애플리케이션 생성
@@ -17,10 +18,19 @@ app.add_middleware(
 # 라우터 등록
 app.include_router(users.router , prefix="/api", tags=["users"])
 
+
+@app.get("/favicon.ico")
+async def favicon():
+    # React가 실행되지 않을 때 기본 favicon 제공 : 404 오류 방지
+    favicon_path = "front/public/favicon.ico"
+    if os.path.exists(favicon_path):
+        return FileResponse(favicon_path)
+    return Response(status_code=204)  # No Content
+
 # 기본 엔드포인트 (테스트용)
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
 # 서버 실행 명령:
-# uvicorn main:app --reload
+# uvicorn main:app
